cfe379364303e8dd35e18babf9423e35cfe2f424
Randy Yoder <yoderrs@outlook.com>
2023-02-01 15:58:23 -0800

    add fix/workaround for expression changed error when accessing the view child table properties from the html

--- workspaces/lib/projects/dev-tests/src/app/pages/selection-examples/selection-examples.component.ts
+++ workspaces/lib/projects/dev-tests/src/app/pages/selection-examples/selection-examples.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { HbMatTable } from '../../../../../../dist/ngx-simple-material-table';
 import { DataTypesEntity } from '../../models/data-types-entity';
@@ -9,7 +9,7 @@ import { DataService } from '../../services/data.service';
   templateUrl: './selection-examples.component.html',
   styleUrls: ['./selection-examples.component.scss']
 })
-export class SelectionExamplesComponent {
+export class SelectionExamplesComponent implements OnInit, AfterViewInit {
     matGridData01: MatTableDataSource<DataTypesEntity>;
     matGridData02: MatTableDataSource<DataTypesEntity>;
     matGridData03: MatTableDataSource<DataTypesEntity>;
@@ -20,12 +20,23 @@ export class SelectionExamplesComponent {
     @ViewChild('multiSelectTable') table02: HbMatTable | undefined;
     @ViewChild('customSelectColorTable') table03: HbMatTable | undefined;
 
-    constructor(private dataService: DataService) {
+    constructor(private dataService: DataService,
+        private changeDetectorRef: ChangeDetectorRef) {
         this.matGridData01 = new MatTableDataSource<DataTypesEntity>(this.dataService.gridData);
         this.matGridData02 = new MatTableDataSource<DataTypesEntity>(this.dataService.gridData);
         this.matGridData03 = new MatTableDataSource<DataTypesEntity>(this.dataService.gridData);
         this.matGridData04 = new MatTableDataSource<DataTypesEntity>(this.dataService.gridData);
         this.matGridData05 = new MatTableDataSource<DataTypesEntity>(this.dataService.gridData);
 
+    }
+    ngOnInit(): void {
+        console.debug(this.table01);
+    }
+    ngAfterViewInit(): void {
+        // the view child properties are undefined initially, and without this referencing the selection directly causes
+        // an Expression Changed error.
+        this.changeDetectorRef.detectChanges();
+
+        console.debug(this.table01);
     }
 }
